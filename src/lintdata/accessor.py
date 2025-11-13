32caff44998178436ee0db6c6c52c105e2c94f02
Heet Patel <heetkpatel30@gmail.com>
2025-11-13 11:16:45 +0000

    feat: integrate data consistency check into report

--- src/lintdata/accessor.py
+++ src/lintdata/accessor.py
@@ -50,6 +50,7 @@ class LintAccessor:
         all_warnings.extend(checks.check_data_type_consistency(self._df))
         all_warnings.extend(checks.check_negative_values(self._df))
         all_warnings.extend(checks.check_rare_categories(self._df))
+        all_warnings.extend(checks.check_date_format_consistency(self._df))
 
         if not all_warnings:
             report_lines.append("No issues found. DataFrame looks good!")
